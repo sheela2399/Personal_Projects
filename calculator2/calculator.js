cf477518f4b8867e674938ed680f143890e0782b
sheela mishra <sheelamishra1999@gmail.com>
2024-05-04 19:23:19 +0530

    social media task done

--- calculator2/calculator.js
+++ calculator2/calculator.js
@@ -1,58 +1,108 @@
-// Addition function 
-function addition(num1,num2){
-    return num1 + num2    
+// // Addition function 
+// function addition(num1,num2){
+//     return num1 + num2    
+// }
+
+// var result = addition(4,3)
+// document.getElementById("add").innerHTML = result
+
+
+
+// // substraction function
+// function substraction(num1,num2){
+//     return num1 - num2    
+// }
+
+// var result = substraction(4,3)
+//  document.getElementById("substract").innerHTML = result
+
+
+// // multiplication function
+// function multiplication(num1, num2){
+//     return num1 * num2
+// }
+
+// var result = multiplication(4,3)
+// document.getElementById("multiply").innerHTML = result
+
+
+// //Divison function
+// function division(num1,num2){
+//     return num1 / num2
+// }
+
+// var result = division(12,4)
+// document.getElementById("divide").innerHTML = result
+
+
+// // percentage function
+// function percentage (num1,num2){
+//     return (num2/num1)*100
+// }
+
+// var result = percentage()
+// document.getElementById("percentage").innerHTML = result
+
+
+
+
+// method1
+
+// var value1 ="";
+// var displayTag = document.getElementById("display");
+
+// function addToDisplay(value){
+//     value1 =  value1 + value;
+//     displayTag.value = "value1";
+//     //  console.log(value1)
+// }
+
+
+// function calculate(){
+//     console.log(eval(value1));
+//     displayTag.value= "value1";
+
+//     value1 ="";
+// }
+
+
+// method2
+
+const displayTag = document.getElementById("display");
+
+function addToDisplay(value) {
+    displayTag.value += value;
 }
- 
-var result = addition(4,3)
-document.getElementById("add").innerHTML = result
 
-// substraction function
-function substraction(num1,num2){
-    return num1 - num2    
+var result = document.getElementById("display").value;
+
+function calculate(){
+    try {
+        displayTag.value = eval(displayTag.value);
+    }
+    catch (error) {
+        displayTag.value = "Error"
+    }
 }
- 
-var result = substraction(4,3)
- document.getElementById("substract").innerHTML = result
-
-// multiplication function
-function multiplication(num1, num2){
-    return num1 * num2
-}
-
-var result = multiplication(4,3)
-document.getElementById("multiply").innerHTML = result
-
-//Divison function
-function division(num1,num2){
-    return num1 / num2
-}
-
-var result = division(12,4)
-document.getElementById("divide").innerHTML = result
-
-// percentage function
-function percentage (num1,num2){
-    return (num2/num1)*100
-}
-
-var result = percentage()
-document.getElementById("percentage").innerHTML = result
-
-
-// display function
-function display(){
-    document.getElementById("display").value += value
-}
-
 
 // clear function
 function clear(){
-    document.getElementById("clear").value = ''
+    displayTag.value = "";
+}
+
+// delete one number function
+function deleteOneNum() {
+    displayTag.value = displayTag.value.tostring().slice(0,-1);
 }
 
 
-function addDisplay(value){
-      var display = value ++ ;
-      document.getElementById
-}
- 
+
+
+
+
+
+
+
+
+
+
